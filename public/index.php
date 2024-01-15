0a33d50abe3f488dcba0eda19b93beaa831607d3
Illia Makarski <69210635+IlyaMak@users.noreply.github.com>
2024-01-15 20:17:22 +0300

    #5. Use an autoload from the composer

--- public/index.php
+++ public/index.php
@@ -6,11 +6,7 @@ use App\Http\Router;
 
 define('PROJECT_ROOT', dirname(__DIR__));
 
-spl_autoload_register(function ($classname) {
-    $classname = str_replace('\\', '/', $classname);
-    require_once(PROJECT_ROOT . "/src/$classname.php");
-});
-
+require_once(PROJECT_ROOT . '/vendor/autoload.php');
 require_once(PROJECT_ROOT . '/config/routes.php');
 
 (new Router)->run();
