ad9b5a0571a672d33bfd710d5177581a42cf350d
Illia Makarski <69210635+IlyaMak@users.noreply.github.com>
2024-01-25 14:45:36 +0300

    #23. Implement a template for the product list page

--- frontend/src/pages/List.jsx
+++ frontend/src/pages/List.jsx
@@ -1,6 +1,15 @@
 import {useEffect, useState} from "react";
 import RestApiClient from "../services/RestApiClient";
 
+const typeContent = (product) => {
+  const specificAttributes = {
+    'dvd': <div>Size: {product.size} MB</div>,
+    'book': <div>Weight: {product.weight}KG</div>,
+    'furniture': <div>Dimension: {product.height}x{product.width}x{product.length}</div>
+  };
+  return specificAttributes[product.type];
+}
+
 export default function List() {
   const [products, setProducts] = useState([]);
   useEffect(() => {
@@ -9,5 +18,21 @@ export default function List() {
       .then((json) => setProducts(json));
   }, []);
 
-  return products.map((product, index) => (<div key={index}>{product.name}</div>));
+  return (
+    <>
+      <h1>Product List</h1>
+      <a href="/add-product">ADD</a>
+      <button id="delete-product-btn">MASS DELETE</button>
+      <hr/>
+      {products.map((product, index) => (
+        <div key={index}>
+          <input type="checkbox" className="delete-checkbox"/>
+          <div>{product.sku}</div>
+          <div>{product.name}</div>
+          <div>{product.price} $</div>
+          <div>{typeContent(product)}</div>
+        </div>
+      ))}
+    </>
+  );
 }
